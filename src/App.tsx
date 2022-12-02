195bf47aadc8bb0b7bf1075e232bb970d5c01ea3
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2022-12-02 13:00:15 -0400

    🎉 INT COMMIT: Init project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+        </a>
+      </div>
+      <h1> React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
