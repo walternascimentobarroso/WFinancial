d1e3716d2c172db16a9b998167fca94ca02aa05a
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2022-12-05 18:00:47 -0400

    ✨ NEW FEATURE: Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import Header from "./components/project/header";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Header title={"WFinancial"} />
+    </div>
+  );
 }
---
+++ src/components/project/header.tsx
@@ -0,0 +1,11 @@
+export default function Header({ title }: any) {
+  return (
+    <header className="bg-indigo-600">
+      <div className="mx-auto max-w-7xl py-12 px-4 lg:flex lg:items-center lg:justify-center lg:py-16 lg:px-8">
+        <h2 className="text-3xl font-bold tracking-tight text-green-300 sm:text-4xl">
+          <span className="block">{title}</span>
+        </h2>
+      </div>
+    </header>
+  );
+}
