560301afeaec4c4d9919d9eac780f07ecfba51f3
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2022-12-05 17:23:17 -0400

    ➕ ADD DEP: Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,3 @@
 export default function App() {
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-        </a>
-      </div>
-      <h1> React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
